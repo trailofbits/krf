114f4d9aeacb26c9461f3cdd27577858c216d480
William Woodruff <william.woodruff@trailofbits.com>
2018-12-29 16:50:15 -0500

    example: link example

---
+++ example/link.c
@@ -0,0 +1,26 @@
+#include "common.h"
+
+int main(int argc, char const *argv[]) {
+  unsigned int i;
+  int fd;
+  const char *filename = "/tmp/krf_link1";
+  const char *linkname = "/tmp/krf_link2";
+
+  creat(filename, 0700);
+
+  for (i = 0;; i++) {
+    if (i % 1000 == 0) {
+      printf("iteration %u...\n", i);
+    }
+
+    if (link(filename, linkname) < 0) {
+      perror("fault!");
+      unlink(filename);
+      exit(errno);
+    }
+
+    unlink(linkname);
+  }
+
+  return 0;
+}
