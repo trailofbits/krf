1311cd124232f8fc59f3a0b1d05b7c4b361fe729
William Woodruff <william@yossarian.net>
2019-03-09 02:00:30 -0500

    krfexec: Ensure that core dumps are enabled

--- src/krfexec/krfexec.c
+++ src/krfexec/krfexec.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/resource.h>
 #include <sys/personality.h>
 #include <unistd.h>
 #include <errno.h>
@@ -11,29 +12,34 @@
 #define PERSONALITY_FILE "/proc/krf/personality"
 
 int main(int argc, char *argv[]) {
-  int fd;
-  char pers_str[33] = {0};
-  unsigned int pers;
-
   if (argc < 2 || !strcmp(argv[1], "-h")) {
     printf("usage: krfexec <command or file> [args]\n");
     return 1;
   }
 
+  int fd;
   if ((fd = open(PERSONALITY_FILE, O_RDONLY)) < 0) {
     err(errno, "open " PERSONALITY_FILE);
   }
 
+  char pers_str[33] = {0};
   if (read(fd, pers_str, sizeof(pers_str) - 1) < 0) {
     err(errno, "read " PERSONALITY_FILE);
   }
 
+  unsigned int pers;
   if (sscanf(pers_str, "%u", &pers) != 1) {
     errx(1, "oddity: personality isn't numeric?");
   }
 
   if (personality(pers | ADDR_NO_RANDOMIZE) < 0) {
     err(errno, "personality");
+  }
+
+  struct rlimit core_limit;
+  core_limit.rlim_cur = core_limit.rlim_max = RLIM_INFINITY;
+  if (setrlimit(RLIMIT_CORE, &core_limit) < 0) {
+    err(errno, "setrlimit");
   }
 
   if (execvp(argv[1], argv + 1) < 0) {
