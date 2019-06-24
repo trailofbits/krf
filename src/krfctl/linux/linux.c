d28dcac8c5349759359598c76b9d4c818c77e634
Henry Wildermuth <15300927+hmwildermuth@users.noreply.github.com>
2019-06-24 15:10:29 -0400

    krfctl: Split freebsd and linux and implement freebsd (#34)
    * krfctl: Split freebsd and linux and implement freebsd
    
    * krfctl: remove extra whitespace
    
    * krfctl: Add EOPNOTSUPP handling to fault_syscall

---
+++ src/krfctl/linux/linux.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <err.h>
+#include <string.h>
+
+#include "../krfctl.h"
+
+/* control will interpret any number larger than its syscall table
+ * as a command to clear all current masks.
+ * it's a good bet that linux will never have 65535 syscalls.
+ */
+#define CLEAR_MAGIC "65535"
+
+#define CONTROL_FILE "/proc/krf/control"
+#define RNG_STATE_FILE "/proc/krf/rng_state"
+#define PROBABILITY_FILE "/proc/krf/probability"
+#define LOG_FAULTS_FILE "/proc/krf/log_faults"
+#define TARGETING_FILE "/proc/krf/targeting"
+
+void fault_syscall(const char *sys_name) {
+  int fd;
+  const char *sys_num;
+
+  /* TODO(ww): Opening the control file once per syscall is
+   * pretty nasty, but I don't like passing a fd around.
+   * Maybe a static variable that we test-and-set?
+   */
+  if ((fd = open(CONTROL_FILE, O_WRONLY)) < 0) {
+    err(errno, "open " CONTROL_FILE);
+  }
+
+  if (!(sys_num = lookup_syscall_number(sys_name))) {
+    errx(1, "couldn't find syscall: %s", sys_name);
+  }
+
+  if (write(fd, sys_num, strlen(sys_num)) < 0) {
+    /* friendly error message on unsupported syscall */
+    if (errno == EOPNOTSUPP) {
+      errx(errno, "faulting for %s unimplemented", sys_name);
+    } else {
+      err(errno, "write " CONTROL_FILE);
+    }
+  }
+
+  close(fd);
+}
+
+void clear_faulty_calls(void) {
+  int fd;
+
+  if ((fd = open(CONTROL_FILE, O_WRONLY)) < 0) {
+    err(errno, "open " CONTROL_FILE);
+  }
+
+  if (write(fd, CLEAR_MAGIC, strlen(CLEAR_MAGIC)) < 0) {
+    err(errno, "write " CONTROL_FILE);
+  }
+
+  close(fd);
+}
+
+void set_rng_state(const char *state) {
+  int fd;
+
+  if ((fd = open(RNG_STATE_FILE, O_WRONLY)) < 0) {
+    err(errno, "open " RNG_STATE_FILE);
+  }
+
+  if (write(fd, state, strlen(state)) < 0) {
+    err(errno, "write " CONTROL_FILE);
+  }
+
+  close(fd);
+}
+
+void set_prob_state(const char *state) {
+  int fd;
+
+  if ((fd = open(PROBABILITY_FILE, O_WRONLY)) < 0) {
+    err(errno, "open " PROBABILITY_FILE);
+  }
+
+  if (write(fd, state, strlen(state)) < 0) {
+    err(errno, "write " CONTROL_FILE);
+  }
+
+  close(fd);
+}
+
+void toggle_fault_logging(void) {
+  int fd;
+  char buf[32] = {0};
+  unsigned int state;
+
+  if ((fd = open(LOG_FAULTS_FILE, O_RDWR)) < 0) {
+    err(errno, "open " LOG_FAULTS_FILE);
+  }
+
+  if (read(fd, buf, sizeof(buf) - 1) < 0) {
+    err(errno, "read " LOG_FAULTS_FILE);
+  }
+
+  if (sscanf(buf, "%u", &state) != 1) {
+    errx(1, "weird logging state: %s", buf);
+  }
+
+  state = !state;
+  memset(buf, 0, sizeof(buf));
+  snprintf(buf, sizeof(buf), "%u", state);
+
+  if (write(fd, buf, strlen(buf)) < 0) {
+    err(errno, "write " LOG_FAULTS_FILE);
+  }
+
+  close(fd);
+}
+
+void set_targeting(unsigned int mode, const char *data) {
+  int fd;
+  char buf[32] = {0};
+  if ((fd = open(TARGETING_FILE, O_WRONLY)) < 0) {
+    err(errno, "open " TARGETING_FILE);
+  }
+
+  if (snprintf(buf, sizeof(buf), "%u %s", mode, data) < 0) {
+    err(errno, "snprintf");
+  }
+
+  if (write(fd, buf, strlen(buf)) < 0) {
+    err(errno, "write " TARGETING_FILE);
+  }
+
+  close(fd);
+}
