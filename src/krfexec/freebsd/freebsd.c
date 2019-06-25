359c1425c56e4134046ca4f475707ac4d99e8b00
William Woodruff <william@yossarian.net>
2019-06-24 23:43:06 -0400

    krfexec/freebsd: Fix assignment

--- src/krfexec/freebsd/freebsd.c
+++ src/krfexec/freebsd/freebsd.c
@@ -20,7 +20,7 @@
 static uid_t guess_original_uid(void) {
   uid_t uid;
 
-  uid_s = getenv("REAL_UID");
+  const char *uid_s = getenv("REAL_UID");
   if (uid_s != NULL) {
     if (sscanf(uid_s, "%u", &uid) != 1) {
       errx(1, "weird REAL_UID value: %s", uid_s);
@@ -28,7 +28,7 @@ static uid_t guess_original_uid(void) {
     return uid;
   }
 
-  const char *uid_s = getenv("SUDO_UID");
+  uid_s = getenv("SUDO_UID");
   if (uid_s != NULL) {
     if (sscanf(uid_s, "%u", &uid) != 1) {
       errx(1, "weird SUDO_UID value: %s", uid_s);
