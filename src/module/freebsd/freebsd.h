4483150192124e62688113800b80e6a6f1e3ab24
Henry Wildermuth <hmwildermuth@gmail.com>
2019-07-11 17:40:12 -0400

    module: Add locks and provide unsafe option

--- src/module/freebsd/freebsd.h
+++ src/module/freebsd/freebsd.h
@@ -1,5 +1,7 @@
 #pragma once
 // FreeBSD specific definitions
+#include <sys/lock.h>
+#include <sys/mutex.h>
 #include "syscalls.h"
 
 #define KRF_SAFE_WRITE(x) x // ???
@@ -8,6 +10,7 @@
 #define KRF_TARGETING_PROTO struct thread *td
 #define KRF_TARGETING_PARMS td
 #define KRF_PERSONALITY(target) (td->td_proc->p_flag2 & (target))
+#ifdef KRF_FREEBSD_UNSAFE_PID_TRAVERSAL
 #define KRF_PID(target)                                                                            \
   ({                                                                                               \
     struct proc *par = td->td_proc;                                                                \
@@ -20,6 +23,23 @@
     } while ((par = par->p_pptr));                                                                 \
     ret;                                                                                           \
   })
+#else // Default: do a check with depth=1 using locks
+#define KRF_PID(target)                                                                            \
+  ({                                                                                               \
+    int ret = 0;                                                                                   \
+    PROC_LOCK(td->td_proc);                                                                        \
+    if (td->td_proc->p_pid == (target)) {                                                          \
+      ret = 1;                                                                                     \
+    } else {                                                                                       \
+      PROC_LOCK(td->td_proc->p_pptr);                                                              \
+      if (td->td_proc->p_pptr->p_pid == (target))                                                  \
+        ret = 1;                                                                                   \
+      PROC_UNLOCK(td->td_proc->p_pptr);                                                            \
+    }                                                                                              \
+    PROC_UNLOCK(td->td_proc);                                                                      \
+    ret;                                                                                           \
+  })
+#endif
 #define KRF_UID(target)                                                                            \
   (td->td_proc->p_ucred->cr_ruid ==                                                                \
    (target)) // Currently using real UID but could use effective UID (cr_uid)
