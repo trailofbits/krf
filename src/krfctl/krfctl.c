88f17e523f80522bc7e12d3f379128274d6df579
William Woodruff <william@trailofbits.com>
2019-10-29 15:11:56 -0400

    krfctl: Avoid backspace chars

--- src/krfctl/krfctl.c
+++ src/krfctl/krfctl.c
@@ -134,10 +134,16 @@ int main(int argc, char *argv[]) {
              " personality, PID, UID, GID, and INODE\n"
              "available profiles (for -P flag):\n"
              " ");
-      for (fault_profile_t *elem = fault_profile_table; elem->profile != NULL; elem++) {
-        printf("%s, ", elem->profile);
+      fault_profile_t *elem = fault_profile_table;
+      while (elem->profile != NULL) {
+        printf("%s", elem->profile);
+
+        elem++;
+        if (elem->profile != NULL) {
+          printf(", ");
+        }
       }
-      printf("\b\b  \n");
+      printf("\n");
       return 1;
     }
     }
