bc9481f750da042f4da363af273ac63b33c82463
银角dai王 <3440433157@qq.com>
2023-07-28 08:15:52 +0800

    fixed minor issue where log_faults switch function was not working

--- src/module/linux/linux.h
+++ src/module/linux/linux.h
@@ -12,10 +12,12 @@
     if (written < 0) {                                                                             \
       printk(KERN_WARNING "krf: snprintf formatting error\n");                                     \
     } else if (written >= KRF_NETLINK_BUF_SIZE) {                                                  \
-      printk(KERN_WARNING "krf: truncated message\n");                                             \
-      krf_netlink_broadcast(krf_log_msg_buf, KRF_NETLINK_BUF_SIZE);                                \
-    } else {                                                                                       \
-      krf_netlink_broadcast(krf_log_msg_buf, written + 1);                                         \
+      printk(KERN_WARNING "krf: truncated message\n");
+      if(krf_log_faults)                                                                           \
+        krf_netlink_broadcast(krf_log_msg_buf, KRF_NETLINK_BUF_SIZE);                              \
+    } else {
+      if(krf_log_faults)                                                                           \
+        krf_netlink_broadcast(krf_log_msg_buf, written + 1);                                       \
     }                                                                                              \
   })
 #define KRF_SYSCALL_TABLE linux_sys_call_table
